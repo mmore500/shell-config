efe4441d1a4942c154b98dfcb53f94bac0115c34
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2026-06-26 15:20:26 -0400

    Sync palette with system light/dark mode

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,20 @@
--- Solarized Light (workbench.colorTheme: "Solarized Light")
+-- Solarized (workbench.colorTheme: "Solarized Light" / "Solarized (dark)")
 return {
   {
-    "maxmx03/solarized.nvim",
+    "f-person/auto-dark-mode.nvim",
     priority = 1000,
+    dependencies = { "maxmx03/solarized.nvim" },
     config = function()
-      vim.o.background = "light"
-      require("solarized").setup({
-        transparent = { enabled = false },
+      local function apply(mode)
+        vim.o.background = mode
+        require("solarized").setup({ transparent = { enabled = false } })
+        vim.cmd.colorscheme("solarized")
+      end
+      require("auto-dark-mode").setup({
+        update_interval = 1000,
+        set_dark_mode = function() apply("dark") end,
+        set_light_mode = function() apply("light") end,
       })
-      vim.cmd.colorscheme("solarized")
     end,
   },
 }
